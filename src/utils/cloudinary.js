087c3459b1c26308bb1df0e4f81f4e5ee7ed313e
ahsan-bhatti-kodexo <ahsan.bhatti@kodexolabs.com>
2025-04-25 21:29:10 +0500

    setup multer and cloudinary for file upload on server.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+  process.env;
+
+// Configuration
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      console.log("No file path provided");
+      return null;
+    }
+    // Upload the file to Cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded to cloudinary
+    console.log("File uploaded to Cloudinary:", response?.url);
+    return response?.url;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // Delete the locally saved temporary file if the upload fails
+    console.error("Error uploading to Cloudinary:", error);
+    throw error;
+  }
+};
+
+export { uploadCloudinary };
