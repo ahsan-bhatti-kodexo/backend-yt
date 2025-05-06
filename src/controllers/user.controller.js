1d75f10960780f5e39b4662512922fa78d606313
ahsan-bhatti-kodexo <ahsan.bhatti@kodexolabs.com>
2025-05-06 18:46:24 +0500

    feat: exclude watch history from user login response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -151,7 +151,7 @@ const loginUser = asyncHandler(async (req, res) => {
   );
 
   const loggedInUser = await User.findById(user._id).select(
-    "-password -refreshToken"
+    "-password -refreshToken -watchHistory"
   );
 
   const option = {
