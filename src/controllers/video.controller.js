5d0c14b0782606ad0195497dc22e77cb955f395d
ahsan-bhatti-kodexo <ahsan.bhatti@kodexolabs.com>
2025-05-06 13:54:39 +0500

    feat: enhance video publishing functionality with validation and cloud upload

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -14,6 +14,52 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
   // TODO: get video, upload to cloudinary, create video
+
+  // check if title and description is provided
+  if (!title || !description) {
+    throw new ApiErrors(400, "Please provide title and description");
+  }
+
+  // upload video and thumbnail to cloudinary
+  const videoFile = req.files?.videoFileUrl[0].path;
+  const thumbnail = req.files?.thumbnailUrl[0].path;
+
+  console.log("videoFile", videoFile, "thumbnail", thumbnail);
+
+  if (!videoFile && !thumbnail) {
+    throw new ApiErrors(400, "Please provide video and thumbnail");
+  }
+
+  const videoFileUrl = await uploadCloudinary(videoFile);
+  const thumbnailUrl = await uploadCloudinary(thumbnail);
+
+  if (!videoFileUrl || !thumbnailUrl) {
+    throw new ApiErrors(
+      400,
+      "Error uploading video or thumbnail to cloudinary"
+    );
+  }
+
+  const duration = videoFileUrl?.duration;
+
+  const owner = req.user._id;
+
+  const videoDetails = await Video.create({
+    title,
+    description,
+    videoFileUrl: videoFileUrl.secure_url,
+    thumbnailUrl: thumbnailUrl.secure_url,
+    duration: duration || 0,
+    views: 0,
+    isPublished: true,
+    owner,
+  });
+
+  console.log("videoDetails", videoDetails);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videoDetails, "Video created successfully"));
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
